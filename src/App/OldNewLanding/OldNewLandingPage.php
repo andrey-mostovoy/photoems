ca20f4300cde4e6b92822ca842d2fbdbe9a40f86
Dmitry Mikheev <d.miheev@topface.com>
2015-10-21 19:24:08 +0300

    Добавил страницу /old-new-landing и изменил лого на главном лендинге

---
+++ src/App/OldNewLanding/OldNewLandingPage.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\OldNewLanding;
+
+use App\MainLanding\MainLandingPage;
+
+/**
+ * Класс описания главной страницы.
+ * @author Andrey Mostovoy <stalk.4.me@gmail.com>
+ */
+class OldNewLandingPage extends MainLandingPage {
+
+}

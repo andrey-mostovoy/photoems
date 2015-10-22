0e4abc86a7cfc40477ee4b1a5f59d2c72cfa1831
Dmitry Mikheev <d.miheev@topface.com>
2015-10-22 12:35:57 +0300

    Revert "Заменил верхний баннер для /old-landing"
    This reverts commit 428660056e5ed4d93d1965c69df4ac94b9083e5a.

--- static/html/old-landing/js/main.js
+++ static/html/old-landing/js/main.js
@@ -2,24 +2,12 @@ var Const = {};
 Const.BANNERS_PATH = 'images/banners/';
 
 (function start() {
-    //var topBannerSpot = document.getElementById('topBannerSpot');
+    var topBannerSpot = document.getElementById('topBannerSpot');
     var bottomBannerSpot = document.getElementById('bottomBannerSpot');
 
-    //var topBannerRotation = new BannerRotation(topBannerSpot, BannerConfig.TOP_BANNERS);
+    var topBannerRotation = new BannerRotation(topBannerSpot, BannerConfig.TOP_BANNERS);
     var bottomBannerRotation = new BannerRotation(bottomBannerSpot, BannerConfig.BOTTOM_BANNERS);
 
-    //topBannerRotation.start();
+    topBannerRotation.start();
     bottomBannerRotation.start();
 })();
-
-(function(nodeId) {
-    var iframe = document.createElement('iframe');
-    iframe.frameBorder = 0;
-    iframe.width = 990;
-    iframe.height = 270;
-    iframe.src = '//topface.com/reg-banner/';
-    iframe.onload = function() {
-        console.log('iframe load!');
-    };
-    document.getElementById(nodeId).appendChild(iframe);
-})('topBannerSpot');
